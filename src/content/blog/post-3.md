b3f19d9e2e1cb004dad6d30bfee72a3dd077408b
munziu263 <munziu@gmail.com>
2023-05-18 08:05:27 +0200

    added basic blog content collection

---
+++ src/content/blog/post-3.md
@@ -0,0 +1,13 @@
+---
+draft: false
+title: Post 3
+description: My third post
+date: 2023-05-20
+author: Munatsi Ziumbe
+# image: ~/data/images/blossoms.jpg
+---
+Lorem ipsum dolor sit amet.
+
+Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati, a rerum! Porro alias consequatur praesentium, assumenda suscipit dolorem eius vitae sequi cupiditate facilis corporis, culpa facere totam veniam. Laudantium, quae.
+
+Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati, a rerum! Porro alias consequatur praesentium, assumenda suscipit dolorem eius vitae sequi cupiditate facilis corporis, culpa facere totam veniam. Laudantium, quae.
---
+++ src/content/config.ts
@@ -0,0 +1,21 @@
+// 1. Import utilities from `astro:content`
+import { z, defineCollection } from "astro:content";
+// 2. Define a schema for each collection you'd like to validate.
+const blog = defineCollection({
+  schema: z.object({
+    draft: z.boolean(),
+    title: z.string().max(60, { message: "Title too long!" }),
+    description: z.string().max(200, { message: "Description too long!" }),
+    date: z.date(),
+    author: z.enum([
+      "Munatsi Ziumbe",
+      "Sophia Imogen Ziumbe",
+      "Kundai Ziumbe ",
+    ]),
+    image: z.string().optional(),
+  }),
+});
+// 3. Export a single `collections` object to register your collection(s)
+export const collections = {
+  blog,
+};
